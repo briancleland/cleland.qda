d35ddc65d857b028c5fefd0bc0fe2f100ce987d3
briancleland <brian_cleland@yahoo.com>
2015-03-04 11:13:42 +0000

    Added bold and italic text functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ define(function (require, exports, module) {
     CommandManager = brackets.getModule("command/CommandManager"),
     Commands = brackets.getModule("command/Commands"),
     DocumentManager = brackets.getModule("document/DocumentManager"),
+    EditorManager = brackets.getModule("editor/EditorManager"),
     PanelManager = brackets.getModule("view/PanelManager"),
     KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
     LanguageManager = brackets.getModule("language/LanguageManager"),
@@ -15,6 +16,8 @@ define(function (require, exports, module) {
   var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
   var SimpleMode = require("lib/codemirror/simple");
 
+  var DO_BOLD = "cleland.qda.boldText";
+  var DO_ITALIC = "cleland.qda.italicText";
   var DO_QDA = "qda.run";
   var panelHtml = require("text!html/panel.html");
   var panel;
@@ -33,12 +36,40 @@ define(function (require, exports, module) {
       _find.hashtags();
     }
   }
-  
+
+  function boldText() {
+    var currentDoc = DocumentManager.getCurrentDocument();
+    var editor = EditorManager.getCurrentFullEditor();
+    var text = editor.getSelectedText();
+    var selection = editor.getSelection();
+    currentDoc.replaceRange("**" + text + "**", selection.start, selection.end);
+  }
+
+  function italicText() {
+    var currentDoc = DocumentManager.getCurrentDocument();
+    var editor = EditorManager.getCurrentFullEditor();
+    var text = editor.getSelectedText();
+    var selection = editor.getSelection();
+    currentDoc.replaceRange("*" + text + "*", selection.start, selection.end);
+  }
+
   CommandManager.register("QDA Browser", DO_QDA, _qdaBrowser);
+  CommandManager.register("Bold Text", DO_BOLD, boldText);
+  CommandManager.register("Italic Text", DO_ITALIC, italicText);
 
   AppInit.appReady(function () {
     var viewMenu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
     viewMenu.addMenuItem(DO_QDA);
+    KeyBindingManager.removeBinding("Cmd-B");
+    KeyBindingManager.removeBinding("Cmd-I");
+    KeyBindingManager.addBinding(DO_BOLD, {
+      key: "Cmd-B",
+      displayKey: "Cmd-B"
+    });
+    KeyBindingManager.addBinding(DO_ITALIC, {
+      key: "Cmd-I",
+      displayKey: "Cmd-I"
+    });
     ExtensionUtils.loadStyleSheet(module, "css/qda.css");
     ExtensionUtils.loadStyleSheet(module, "lib/jqtree/jqtree.css");
     ExtensionUtils.loadStyleSheet(module, "css/font-awesome/css/font-awesome.min.css");
